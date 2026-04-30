4c1aa480307ab957d77efa27d288030e294359de
2Andrew6 <pavel240313@gmail.com>
2026-04-30 12:02:06 +0300

    ozon ozon

---
+++ dfgdgdgs.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const legalEntitySchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: [ true, 'Название юридического лица'],
+        trim: true,
+        unique: true
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+const LegalEntity = mongoose.model('LegalEntity', legalEntitySchema);
+
+module.exports = LegalEntity;
