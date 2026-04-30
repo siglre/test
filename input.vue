912b18137c0ec5a5e3f6c952b9e023bbf8aa347d
Ilya <166129066+siglre@users.noreply.github.com>
2026-04-30 11:51:38 +0300

    Add files via upload

---
+++ input.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  id: String,
+  type: {
+    type: String,
+    default: 'text',
+  },
+  placeholder: String,
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const onInput = event => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+  <label class="field" :for="id">
+    <span class="field__label">{{ label }}</span>
+    <input
+      :id="id"
+      class="field__input"
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="onInput"
+    />
+  </label>
+</template>
---
+++ label.vue
@@ -0,0 +1,8 @@
+<template>
+  <p class="disclaimer">
+    Нажимая «Войти», вы принимаете
+    <a href="#" target="_blank">условия</a>
+    и
+    <a href="#" target="_blank">политику конфиденциальности</a>.
+  </p>
+</template>
